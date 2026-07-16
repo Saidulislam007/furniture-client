592963046efe05709b58ce915906f4bc12b4036e
Saidul Islam <youremail@example.com>
2026-07-16 09:22:34 +0600

    done

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
-
+baseURL: process.env.BETTER_AUTH_URL,
 const uri = process.env.MONGODB_URI;
 
 if (!uri) {
